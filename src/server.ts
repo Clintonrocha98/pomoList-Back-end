61b471cd337ce60da391ce1839e10fbb2c226bf8
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2023-09-25 17:49:55 -0300

    cors add

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
+
 const port = process.env.PORT || 3333;
 
+app.use(cors({ origin: true }));
 
 app.use(express.json());
 
