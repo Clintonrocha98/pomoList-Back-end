bfef2e57fac0ecfd89d384e0daf583f948197ef4
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2023-10-27 15:50:07 -0300

    feat: async error handling middleware

--- src/middleware/ensuredAuthenticated.ts
+++ src/middleware/ensuredAuthenticated.ts
@@ -1,23 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { decode, verify } from "jsonwebtoken";
+import { UnauthorizedError } from "../helpers/api-errors";
 
 export const ensuredAuthenticated = () => {
   return async (request: Request, response: Response, next: NextFunction) => {
     const authHeaders = request.headers.authorization;
 
     if (!authHeaders) {
-      return response.status(401).json({ error: "Token is missing." });
+      throw new UnauthorizedError("Token is missing.");
     }
     const [, token] = authHeaders.split(" ");
 
-    try {
-      verify(token, process.env.SECRET_JWT);
+    verify(token, process.env.SECRET_JWT);
 
-      const { sub } = decode(token);
-      request.userId = sub.toString();
-      return next();
-    } catch (error) {
-      return response.status(401).end();
-    }
+    const { sub } = decode(token);
+    request.userId = sub.toString();
+    return next();
   };
 };
